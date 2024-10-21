778deed5acd85eb0b0ff24d6ce87046d16d2ca62
Lucas Anael <llucasanael@gmail.com>
2024-10-21 08:34:27 -0300

    cadastro colaborador

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -6,7 +6,7 @@ const alunosController = require('../controller/alunosController.js');
 const livrosController = require('../controller/livrosController.js'); 
 const emprestarController = require('../controller/emprestarController.js'); 
 const reservaController  = require ('../controller/reservaController.js')
-const cadastroUsuarioController = require('../controller/cadastroUsuarioController.js'); 
+const cadastroUsuarioController = require('../controller/cadastroColaboradorController.js'); 
 
 
 router.get('/alunos/:rm', alunosController.getAluno);
@@ -17,7 +17,7 @@ router.get('/buscaracervo/:Exemplar', livrosController.buscarPorExemplar);
 
 router.put('/atualizaracervo/:Exemplar', livrosController.atualizarAcervo);
 
-router.post('/registrarusuario', cadastroUsuarioController.createUsuario);
+router.post('/registrarcolaborador', cadastroUsuarioController.createUsuario);
 
 router.post('/reservar', reservaController.registrarReserva)
 
